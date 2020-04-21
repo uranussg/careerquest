c04763db86ec5f7d51604c2f2a9c234167f0e81a
uranussg <uranussg@gmail.com>
2020-04-21 10:58:48 -0700

    bst

--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -3,5 +3,12 @@
 
 
 function isBalanced(root) {
+    if (!root) return true
+    return isBalanced(root.left) && isBalanced(root.right) && Math.abs(height(root.left) - height(root.right)) < 2
+}
 
+
+function height(root, h = 0) {
+    if (!root) return -1
+    return Math.max(root.left, root.right) +1
 }
